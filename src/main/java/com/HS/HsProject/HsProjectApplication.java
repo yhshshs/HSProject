c4f7662a7b8b0ff2b1ac7a4f979951d5c647fd79
YHS <YHS@yc.ac.kr>
2023-05-04 22:45:13 +0900

    base

---
+++ src/main/java/com/HS/HsProject/HsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.HS.HsProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HsProjectApplication.class, args);
+	}
+
+}
